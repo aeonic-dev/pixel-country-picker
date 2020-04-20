92d5dec27b47d7ffcfd5b0031ed26d57f541620f
query <ericknwash@gmail.com>
2020-04-20 10:03:41 -0500

    Added list-flags, restructured

---
+++ list-flags/index.js
@@ -0,0 +1,19 @@
+x = 15;
+y = 9;
+fi = -1;
+flagNodes = document.querySelector('.flags');
+for (i = 0; i < flags.length - 1; i++) {
+  flagNodes.appendChild(document.createElement('div')).classList.add('flag');
+  flagNodes.appendChild(document.createElement('p')).innerHTML=flagnames[i];
+  flagNodes.appendChild(document.createElement('br'));
+}
+let divs = document.querySelectorAll('.flag');
+divs.forEach(n=>{
+  fi ++;
+  for (i = 0; i < x * y; i++) {
+    s=n.appendChild(document.createElement("span"));
+    s.classList.add("pixel");
+    s.style.background=flags[fi][i];
+  }
+
+});
